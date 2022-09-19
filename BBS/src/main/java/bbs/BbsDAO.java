31f1c2baf0d63286b5aef9473412bbe5021581d6
gkseogus <fbzbffldj@gmail.com>
2022-09-19 18:34:05 +0900

    220919_add to bbsDAO class

---
+++ BBS/src/main/java/bbs/BbsDAO.java
@@ -0,0 +1,120 @@
+package bbs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class BbsDAO {
+	// ctrl + shift + o 로 외부 라이브러리를 한번에 불러올 수 있다.
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	// mariadb 접속
+	public BbsDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/userDB";
+			String dbID = "root";
+			String dbPassword = "gkseogus";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 현재의 시간을 가져오는 함수
+	public String getDate() {
+		String SQL = "SELECT NOW()";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return rs.getString(1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 데이터 베이스 오류
+		return "";
+	}
+
+	// 게시물 순서 함수
+	public int getNext() {
+		String SQL = "SELECT bbsID FROM bbs ORDER BY bbsID DESC";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return rs.getInt(1) + 1;
+			}
+			// 첫 번째 게시물인 경우
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 데이터 베이스 오류
+		return -1;
+	}
+
+	// 글을 작성하는 함수
+	public int write(String bbsTitle, String userID, String bbsContent) {
+		String SQL = "INSERT INTO bbs VALUES (?, ?, ?, ?, ?, ?)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext());
+			pstmt.setString(2, bbsTitle);
+			pstmt.setString(3, userID);
+			pstmt.setString(4, getDate());
+			pstmt.setString(5, bbsContent);
+			pstmt.setInt(6, 1);
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 데이터 베이스 오류
+		return -1;
+	}
+
+	// 특정한 페이지에 맞는 게시글을 노출
+	public ArrayList<Bbs> getList(int pageNumber) {
+		String SQL = "SELECT * FROM bbs WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		ArrayList<Bbs> list = new ArrayList<Bbs>();
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				Bbs bbs = new Bbs();
+				bbs.setBbsID(rs.getInt(1));
+				bbs.setBbsTitle(rs.getString(2));
+				bbs.setUserID(rs.getString(3));
+				bbs.setBbsDate(rs.getString(4));
+				bbs.setBbsContent(rs.getString(5));
+				bbs.setBbsAvailable(rs.getInt(6));
+				list.add(bbs);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	// 페이징 처리 (ex -> 게시글 10개 = 페이지 1개, 게시글 11개 = 페이지 2개 ...)
+	public boolean nextPage(int pageNumber) {
+		String SQL = "SELECT * FROM bbs WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, getNext() - (pageNumber - 1) * 10);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+}
