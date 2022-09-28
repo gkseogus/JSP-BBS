b0a7441a38545ac6c895d8c12dd6c04ab80e4ce1
gkseogus <fbzbffldj@gmail.com>
2022-09-28 11:07:28 +0900

    220928_apply to 최근 데이터 함수

--- BBS/src/main/java/chat/ChatDAO.java
+++ BBS/src/main/java/chat/ChatDAO.java
@@ -12,6 +12,7 @@ public class ChatDAO {
 	private PreparedStatement pstmt;
 	private ResultSet rs;
 
+	// _mariadb 접속 함수
 	public ChatDAO() {
 		try {
 			String dbURL = "jdbc:mysql://localhost:3306/ANONYMOUSCHAT";
@@ -35,6 +36,7 @@ public class ChatDAO {
 			chatList = new ArrayList<Chat>();
 			while (rs.next()) {
 				Chat chat = new Chat();
+				chat.setChatID(rs.getInt("chatID"));
 				chat.setChatName(rs.getString("chatName"));
 				chat.setChatContent(rs.getString("chatContent").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;")
 						.replaceAll(">", "&gt;").replaceAll("" + "\n", "<br>"));
@@ -63,9 +65,89 @@ public class ChatDAO {
 		return chatList;
 	}
 
+	// 최근 몇 개의 데이터만 불러오는 함수
+	public ArrayList<Chat> getChatListByRecent(int number) {
+		ArrayList<Chat> chatList = null;
+		String SQL = "SELECT * FROM CHAT WHERE chatID > (SELECT MAX(chatID) - ? FROM CHAT) ORDER BY chatTime";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, number);
+			rs = pstmt.executeQuery();
+			chatList = new ArrayList<Chat>();
+			while (rs.next()) {
+				Chat chat = new Chat();
+				chat.setChatID(rs.getInt("chatID"));
+				chat.setChatName(rs.getString("chatName"));
+				chat.setChatContent(rs.getString("chatContent").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;")
+						.replaceAll(">", "&gt;").replaceAll("" + "\n", "<br>"));
+				int chatTime = Integer.parseInt(rs.getString("chatTime").substring(11, 13));
+				String timeType = "am";
+				if (Integer.parseInt(rs.getString("chatTime").substring(11, 13)) >= 12) {
+					timeType = "pm";
+					chatTime -= 12;
+				}
+				chat.setChatTime(rs.getString("chatTime").substring(0, 11) + " " + timeType + " " + chatTime + ":"
+						+ rs.getString("chatTime").substring(14, 16) + "");
+				chatList.add(chat);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return chatList;
+	}
+
+	// 특정한 id보다 큰 id를 모두 추출하는 함수
+	public ArrayList<Chat> getChatListByRecent(String chatID) {
+		ArrayList<Chat> chatList = null;
+		String SQL = "SELECT * FROM CHAT WHERE chatID > ? ORDE BY chatTime";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, Integer.parseInt(chatID));
+			rs = pstmt.executeQuery();
+			chatList = new ArrayList<Chat>();
+			while (rs.next()) {
+				Chat chat = new Chat();
+				chat.setChatID(rs.getInt("chatID"));
+				chat.setChatName(rs.getString("chatName"));
+				chat.setChatContent(rs.getString("chatContent").replaceAll(" ", "&nbsp;").replaceAll("<", "&lt;")
+						.replaceAll(">", "&gt;").replaceAll("" + "\n", "<br>"));
+				int chatTime = Integer.parseInt(rs.getString("chatTime").substring(11, 13));
+				String timeType = "am";
+				if (Integer.parseInt(rs.getString("chatTime").substring(11, 13)) >= 12) {
+					timeType = "pm";
+					chatTime -= 12;
+				}
+				chat.setChatTime(rs.getString("chatTime").substring(0, 11) + " " + timeType + " " + chatTime + ":"
+						+ rs.getString("chatTime").substring(14, 16) + "");
+				chatList.add(chat);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return chatList;
+	}
+
 	// 사용자가 메시지를 입력하도록 하는 함수
 	public int submit(String chatName, String chatContent) {
-		String SQL = "INSERT INTO CHAT VALUES (?, ?, now())";
+		String SQL = "INSERT INTO CHAT VALUES (NULL, ?, ?, now())";
 		try {
 			pstmt = conn.prepareStatement(SQL);
 			pstmt.setString(1, chatName);
