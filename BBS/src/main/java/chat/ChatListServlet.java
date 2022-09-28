4f50111f7dc839d96940f24a61e8e7c2f30b359e
gkseogus <fbzbffldj@gmail.com>
2022-09-28 11:08:13 +0900

    220928_apply to 데이터 불러오는 함수

--- BBS/src/main/java/chat/ChatListServlet.java
+++ BBS/src/main/java/chat/ChatListServlet.java
@@ -24,6 +24,15 @@ public class ChatListServlet extends HttpServlet {
 			response.getWriter().write("");
 		} else if (listType.equals("today")) {
 			response.getWriter().write(getToday());
+		} else if (listType.equals("ten")) {
+			response.getWriter().write(getTen());
+		} else {
+			try {
+				Integer.parseInt(listType);
+				response.getWriter().write(getID(listType));
+			} catch (Exception e) {
+				response.getWriter().write("");
+			}
 		}
 	}
 
@@ -42,7 +51,41 @@ public class ChatListServlet extends HttpServlet {
 			if (i != chatList.size() - 1)
 				result.append(",");
 		}
-		result.append("]}");
+		result.append("], \"last\":\"" + chatList.get(chatList.size() - 1).getChatID() + "\"}");
+		return result.toString();
+	}
+
+	// 최근 10개의 데이터를 가져오는 함수
+	public String getTen() {
+		StringBuffer result = new StringBuffer("");
+		result.append("{\"result\":[");
+		ChatDAO chatDAO = new ChatDAO();
+		ArrayList<Chat> chatList = chatDAO.getChatListByRecent(10);
+		for (int i = 0; i < chatList.size(); i++) {
+			result.append("[{\"value\": \"" + chatList.get(i).getChatName() + "\"},");
+			result.append("{\"value\": \"" + chatList.get(i).getChatContent() + "\"},");
+			result.append("{\"value\": \"" + chatList.get(i).getChatTime() + "\"}]");
+			if (i != chatList.size() - 1)
+				result.append(",");
+		}
+		result.append("], \"last\":\"" + chatList.get(chatList.size() - 1).getChatID() + "\"}");
+		return result.toString();
+	}
+
+	// 특정한 id 이후의 값을 가져오는 함수
+	public String getID(String chatID) {
+		StringBuffer result = new StringBuffer("");
+		result.append("{\"result\":[");
+		ChatDAO chatDAO = new ChatDAO();
+		ArrayList<Chat> chatList = chatDAO.getChatListByRecent(chatID);
+		for (int i = 0; i < chatList.size(); i++) {
+			result.append("[{\"value\": \"" + chatList.get(i).getChatName() + "\"},");
+			result.append("{\"value\": \"" + chatList.get(i).getChatContent() + "\"},");
+			result.append("{\"value\": \"" + chatList.get(i).getChatTime() + "\"}]");
+			if (i != chatList.size() - 1)
+				result.append(",");
+		}
+		result.append("], \"last\":\"" + chatList.get(chatList.size() - 1).getChatID() + "\"}");
 		return result.toString();
 	}
 }
